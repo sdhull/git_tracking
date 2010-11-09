61570c839c89feea79d939fd47259568a6eff28c
Steve Hull <p.witty@gmail.com>
2010-11-08 19:00:05 -0800

    6132035 Detect should ignore .git_tracking

--- lib/git_tracking/detect.rb
+++ lib/git_tracking/detect.rb
@@ -1,8 +1,8 @@
 class GitTracking
   class << self
     def detect_debuggers
-      file_names = `git diff-index --cached -Sdebugger --name-only HEAD`.chomp
-      file_names = file_names.gsub(".git_tracking\n",'')
+      file_names = `git diff-index --cached -Sdebugger --name-only HEAD`
+      file_names = file_names.gsub(".git_tracking",'').strip
       if file_names != ""
         highline.say highline.color("The following files have 'debugger' statements in them: ", :red)
         highline.say file_names
